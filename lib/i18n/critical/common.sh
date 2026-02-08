014c217e84f579e453939b21e8f797398bd10904
Den <denor81@ya.ru>
2026-02-08 23:30:18 +0300

    add some critical messages

--- lib/i18n/critical/common.sh
+++ lib/i18n/critical/common.sh
@@ -14,3 +14,16 @@ I18N_MESSAGES["common.helpers.rollback.int_received"]="Received INT signal [RC: 
 I18N_MESSAGES["common.error_root_privileges"]="Root privileges or run with 'sudo' required. Running as regular user."
 I18N_MESSAGES["common.error_invalid_input"]="Input error. Expected: %s"
 I18N_MESSAGES["common.loaded"]="Loaded [%s]"
+
+# Init messages
+I18N_MESSAGES["init.gawk.version"]="Critical dependencies:"
+I18N_MESSAGES["init.gawk.installed"]="gawk installed [%s]"
+I18N_MESSAGES["init.gawk.not_installed"]="gawk not installed"
+I18N_MESSAGES["init.gawk.nul_explanation"]="gawk is required for associative arrays in Bash"
+I18N_MESSAGES["init.gawk.install_confirm"]="Install gawk?"
+I18N_MESSAGES["init.gawk.install_success"]="gawk installed successfully"
+I18N_MESSAGES["init.gawk.install_error"]="Failed to install gawk"
+
+# IO messages
+I18N_MESSAGES["io.confirm_action.default_question"]="Continue?"
+I18N_MESSAGES["io.confirm_action.run_setup"]="Run setup?"
