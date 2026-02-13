d7445fd7daef87f7439d2c3132dd7cff2ce00bde
Den <denor81@ya.ru>
2026-02-13 16:06:24 +0300

    process

--- modules/user-modify.sh
+++ modules/user-modify.sh
@@ -28,6 +28,13 @@ user::orchestrator::create_dispatcher() {
     printf '%s:%s' "$BSSS_USER_NAME" "$password"
 }
 
+user_info_del_block() {
+    log_info "Напоминание, как удалить пользователя:"
+    log_info_simple_tab "deluser --remove-home --remove-all-files USERNAME # Удалить пользователя"
+    log_info_simple_tab "find / -uid USERID 2>/dev/null # Проверка найти все созданные файлы по id"
+    log_info_simple_tab "pgrep -u USERNAME # посмотреть PID процессов"
+    log_info_simple_tab "killall -9 -u USERNAME # завершить все процессы"
+}
 # @type:        Orchestrator
 # @description: Создает пользователя BSSS
 # @params:      нет
@@ -37,14 +44,14 @@ user::orchestrator::create_dispatcher() {
 #               $? - ошибка при создании пользователя
 user::orchestrator::create_user() {
 
-    log_attention "Не закрывайте это окно терминала"
     log_info "Что будет происходить:"
     log_info_simple_tab "Создание: [useradd -m -d /home/$BSSS_USER_NAME -s /bin/bash -G sudo $BSSS_USER_NAME]"
     log_info_simple_tab "Генерация пароля: [openssl rand -base64 $BSSS_USER_PASS_LEN]"
     log_info_simple_tab "Создание правил в ${SUDOERS_D_DIR}/${BSSS_USER_NAME}"
     log_info_simple_tab "Пароль будет выведен только раз на экран терминала (в логи не пишется)"
-    log_info "После создания пользователя необходимо скопировать ваш ключ командой ssh-copy-id"
+    log_info "После создания пользователя необходимо скопировать ваш SSH ключ на сервер командой ssh-copy-id"
     log_info "Проверить авторизацию по ключу и если все ок, то можно запрещать доступ по паролю и доступ от имени root"
+    user_info_del_block
 
     io::confirm_action "Создать пользователя $BSSS_USER_NAME?"
     local cred
@@ -55,36 +62,28 @@ user::orchestrator::create_user() {
     log_info "Проверьте возможность авторизации по логину и паролю"
     log_info "Скопируйте на сервер ключ для подключения по SSH [ssh-copy-id]"
     log_info "После копирования SSH ключа и успешного подключения можно будет запретить авторизацию по паролю"
-    log_info "Напоминание, как удалить пользователя:"
-    log_info_simple_tab "deluser --remove-home --remove-all-files USERNAME # Удалить пользователя"
-    log_info_simple_tab "find / -uid USERID 2>/dev/null # Проверка найти все созданные файлы по id"
-    log_info_simple_tab "pgrep -u USERNAME # посмотреть PID процессов"
-    log_info_simple_tab "killall -9 -u USERNAME # завершить все процессы"
 }
 
 user::orchestrator::need_add_bsssuser() {
     log_info "В системе один единственный пользователь root"
-    log_info "Настоятельно рекомендуется создать второго пользователя"
+    log_info "Настоятельно рекомендуется создать второго пользователя с правами sudo"
     user::info::block
-    log_info "Вы можете создать пользователя $BSSS_USER_NAME автоматически или создать любого другого пользователя вручную"
+    log_info "Вы можете создать пользователя $BSSS_USER_NAME автоматически (права будут добавлены) или создать любого другого пользователя самостоятельно"
     user::orchestrator::create_user
-    log_info "Убедитесь, что вы можете подключиться по SSH ключу под нужным пользователем (не root), затем запрещайте авторизацию по паролю и доступ от root пользователя"
 }
 
 user::orchestrator::can_add_bsssuser() {
     log_info "Помимо пользователя root уже созданы другие пользователи"
-    log_info "Авторизируйтесь по SSH ключу под пользователем отличным от root, чтобы отключить возможность авторизации по паролю и авторизацию под root"
+    log_info "Можно создать отдельного пользователя $BSSS_USER_NAME"
     user::info::block
-    log_info "Можно создать отдельного пользователя $BSSS_USER_NAME или авторизироваться под уже созданным"
     user::orchestrator::create_user
-    log_info "Убедитесь, что вы можете подключиться по SSH ключу под нужным пользователем (не root), затем запрещайте авторизацию по паролю и доступ от root пользователя"
 }
 
 user::log::no_new_user_needed() {
     log_info "Пользователь $BSSS_USER_NAME уже создан"
-    log_info "Созание дополнительного пользователя возможно только в ручную"
+    log_info "Созание дополнительного пользователя возможно только вручную"
     user::info::block
-    log_info "Убедитесь, что вы можете подключиться по SSH ключу под нужным пользователем (не root), затем запрещайте авторизацию по паролю и доступ от root пользователя"
+    user_info_del_block
 }
 
 user::dispatch::logic() {
